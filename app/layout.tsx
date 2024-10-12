e547854dbc61d493b75b776c816181d308f42bbd
yusufAbdulrasheed <yusufabdulrasheed200@gmail.com>
2024-10-12 15:40:56 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next"
+import { Navbar, Footer } from "@/components"
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Car Showcase",
+  description: "Discover the best cars in the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className='relative'>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
+    </html>
+  );
+}
